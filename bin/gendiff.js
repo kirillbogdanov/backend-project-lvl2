d3de6bee3a58726be4c661c7e964a0422c838f7f
Kirill Bogdanov <kiralbogdanov@gmail.com>
2020-06-04 17:42:36 +0300

    Part 3 completed

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 import commander from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new commander.Command();
 
 program
-  .arguments('<filepath1> <filepath2>')
+  .arguments('<filePath1> <filePath2>')
   .option('-f, --format [type]', 'output format')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
-  .action((filepath1, filepath2) => {
-    console.log(filepath1);
-    console.log(filepath2);
+  .action((filePath1, filePath2) => {
+    console.log(genDiff(filePath1, filePath2));
   });
 
 program.parse(process.argv);
