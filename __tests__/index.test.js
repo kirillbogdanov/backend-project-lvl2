28ab9ffcf33b41993215124cffb0392498cbfc19
Kirill Bogdanov <kiralbogdanov@gmail.com>
2020-06-11 13:53:19 +0300

    Part 7 completed

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,21 +4,21 @@ import genDiff from './index.js';
 
 test('2 flat json files', () => {
   const expected = _.trimEnd(readFileSync(`${__dirname}/../__fixtures__/result.txt`).toString(), '\n');
-  const result = genDiff(`${__dirname}/../__fixtures__/before.json`, `${__dirname}/../__fixtures__/after.json`);
+  const result = genDiff(`${__dirname}/../__fixtures__/before.json`, `${__dirname}/../__fixtures__/after.json`, 'stylish');
 
   expect(result).toEqual(expected);
 });
 
 test('2 flat yml files', () => {
   const expected = _.trimEnd(readFileSync(`${__dirname}/../__fixtures__/result.txt`).toString(), '\n');
-  const result = genDiff(`${__dirname}/../__fixtures__/before.yml`, `${__dirname}/../__fixtures__/after.yml`);
+  const result = genDiff(`${__dirname}/../__fixtures__/before.yml`, `${__dirname}/../__fixtures__/after.yml`, 'stylish');
 
   expect(result).toEqual(expected);
 });
 
 test('2 flat ini files', () => {
   const expected = _.trimEnd(readFileSync(`${__dirname}/../__fixtures__/result.txt`).toString(), '\n');
-  const result = genDiff(`${__dirname}/../__fixtures__/before.ini`, `${__dirname}/../__fixtures__/after.ini`);
+  const result = genDiff(`${__dirname}/../__fixtures__/before.ini`, `${__dirname}/../__fixtures__/after.ini`, 'stylish');
 
   expect(result).toEqual(expected);
 });
